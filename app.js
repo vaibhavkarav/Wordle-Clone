71556d80696ff0ba4a070630ea7546a1a54b5ecd
Vaibhav Karav <vaibhavkarav99@gmail.com>
2022-12-06 21:27:36 +0530

    add colors

--- app.js
+++ app.js
@@ -117,6 +117,7 @@ const checkRow = () => {
   const guess = guessRows[currentRow].join("");
   if (currentTile > 4) {
     console.log(`Guess is ${guess}, Wordle is ${wordle}.`);
+    flipTile();
     if (wordle === guess) {
       showMessage("Magnificent!");
       isGameOver = true;
@@ -140,4 +141,19 @@ const showMessage = (message) => {
   messageElement.textContent = message;
   messageDisplay.append(messageElement);
   setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
+};
+
+const flipTile = () => {
+  const rowTiles = document.querySelector(`#guessRow-${currentRow}`).childNodes;
+  rowTiles.forEach((tile, index) => {
+    const dataLetter = tile.getAttribute("data");
+
+    if (dataLetter === wordle[index]) {
+      tile.classList.add("green-overlay");
+    } else if (wordle.includes(dataLetter)) {
+      tile.classList.add("yellow-overlay");
+    } else {
+      tile.classList.add("grey-overlay");
+    }
+  });
 };
