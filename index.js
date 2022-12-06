6d97250301830739dd854dcafc82c16e93dd7ae7
Vaibhav Karav <vaibhavkarav99@gmail.com>
2022-12-06 22:48:26 +0530

    add required code

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const PORT = 8000;
+const axios = require("axios");
+const express = require("express");
+
+const app = express();
+
+app.get("/word", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://random-words5.p.rapidapi.com/getMultipleRandom",
+    params: { count: "5", wordLength: "5" },
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": "random-words5.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      res.json(response.data[0]);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
