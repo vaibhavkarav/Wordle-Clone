6f0de075eb88afdb5727a251279b3a9cd302519a
Vaibhav Karav <vaibhavkarav99@gmail.com>
2022-12-06 23:06:27 +0530

    fetch the wordle

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 "use strict";
 
+const { response } = require("express");
+
 const tileDisplay = document.querySelector(".tile-container");
 const keyboard = document.querySelector(".key-container");
 const messageDisplay = document.querySelector(".message-container");
 
-const wordle = "SUPER";
+let wordle;
+
+const getWordle = () => {
+  fetch("http://localhost:8000/word")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      wordle = json.toUpperCase();
+    })
+    .catch((err) => console.log(err));
+};
+
+getWordle();
 
 const keys = [
   "Q",
