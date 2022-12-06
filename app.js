d7f6221e59d1b5e8073f323b172075a7f8c6e269
Vaibhav Karav <vaibhavkarav99@gmail.com>
2022-12-06 20:56:07 +0530

    set timeout

--- app.js
+++ app.js
@@ -126,4 +126,5 @@ const showMessage = (message) => {
   const messageElement = document.createElement("p");
   messageElement.textContent = message;
   messageDisplay.append(messageElement);
+  setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
 };
